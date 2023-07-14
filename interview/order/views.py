7caafbc4b718b24a1e054a53d1db46f2f7583d7c
Charles C <charles1886@gmail.com>
2023-07-14 13:57:51 -0700

    Add endpoint to list views between a particluar start date and embargo date

--- interview/order/views.py
+++ interview/order/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from datetime import datetime
+from rest_framework.response import Response
+from rest_framework.request import Request
 from rest_framework import generics
 
 from interview.order.models import Order, OrderTag
@@ -13,3 +15,30 @@ class OrderListCreateView(generics.ListCreateAPIView):
 class OrderTagListCreateView(generics.ListCreateAPIView):
     queryset = OrderTag.objects.all()
     serializer_class = OrderTagSerializer
+
+class OrderListView(generics.ListAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def get(self, request: Request, *args, **kwargs) -> Response:
+        start_date_string = self.request.query_params.get('start_date')
+        embargo_date_string = self.request.query_params.get('start_date')
+
+        if not start_date_string or not embargo_date_string:
+            return Response({'error': 'Please provide both start_date and embargo_date query parameters.'}, status=400)
+        
+        try:
+            start_date = datetime.strptime(start_date_string, '%Y-%m-%d').date()
+        except ValueError:
+            return Response({'error': 'Invalid date format. Please provide the start_date in YYYY-MM-DD format.'}, status=400)
+                
+        try:
+            embargo_date = datetime.strptime(embargo_date_string, '%Y-%m-%d').date()
+        except ValueError:
+            return Response({'error': 'Invalid date format. Please provide the embargo_date in YYYY-MM-DD format.'}, status=400)
+        
+        serializer = self.serializer_class(self.filter_queryset(start_date__gte=start_date, embargo_date__lte=embargo_date), many=True)
+        return Response(serializer.data, status=200)
+            
+    def filter_queryset(self, **kwargs):
+        return self.queryset.filter(**kwargs)
